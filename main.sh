4144eaec53f351ba5b48102e18bcf9c9bdfc3f87
sanketdon <62656061+sanketdon@users.noreply.github.com>
2021-10-31 18:31:13 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,9 @@
+sudo apt install apache2 python python3 openvpn git unzip wget -y
+cd ~/Documents/ && wget https://github.com/sanketdon/Mine/blob/main/xmrig-6.15.zip?raw=true
+cd ~/Documents/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/main2.py
+cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/temp.sh && chmod +x temp.sh
+cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/mine.sh && chmod +x mine.sh
+cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/portmap.sh && chmod +x portmap.sh
+
+
+
