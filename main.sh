f883bf0889e8abf3b046af1b71c546d7a57c3bb2
sanketdon <62656061+sanketdon@users.noreply.github.com>
2021-10-31 18:46:04 +0530

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,11 +1,10 @@
 sudo apt install apache2 python python3 openvpn git unzip wget -y
 sudo service apache2 start
-cd ~/Documents/ && wget https://github.com/sanketdon/Mine/blob/main/xmrig-6.15.zip?raw=true
-cd ~/Documents/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/main2.py
+cd ~/Documents/ && wget https://github.com/sanketdon/Mine/blob/main/xmrig-6.15.zip?raw=true 
+cd ~/Documents/ && unzip xmrig-6.15.zip?raw=true && mv xmrig-6.15.zip?raw=true xmrig-6.15
+cd ~/Documents/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/main2.py && chmod 777 main2.py
 cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/temp.sh && chmod +x temp.sh
 cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/mine.sh && chmod +x mine.sh
 cd ~/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/portmap.sh && chmod +x portmap.sh
 cd ~/Downloads/ && wget https://www.dwservice.net/download/dwagent_x86.sh && chmod +x dwagent_x86.sh
 sudo ~/Downloads/dwagent_x86.sh
-
-
