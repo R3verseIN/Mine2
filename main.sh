25b5ef8964f001f6b7286c54dd1e75d029643dd1
sanketdon <62656061+sanketdon@users.noreply.github.com>
2021-11-02 10:43:25 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 sudo apt install apache2 python python3 openvpn git unzip wget -y
 sudo service apache2 start
-cd /home/jbl/Documents/ && wget https://github.com/sanketdon/Mine/blob/main/xmrig-6.15.zip?raw=true 
-cd /home/jbl/Documents/ && unzip xmrig-6.15.zip?raw=true && mv xmrig-6.15.zip?raw=true xmrig-6.15
+cd ~/Documents/ && wget https://github.com/sanketdon/Mine/blob/main/xmrig-6.15.zip?raw=true 
+cd ~/Documents/ && unzip xmrig-6.15.zip?raw=true && mv xmrig-6.15.zip?raw=true xmrig-6.15
 cd /home/jbl/Documents/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/main2.py && chmod 777 main2.py
 cd /home/jbl/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/temp.sh && chmod +x temp.sh
 cd /home/jbl/Desktop/ && wget https://raw.githubusercontent.com/sanketdon/Mine/main/mine.sh && chmod +x mine.sh
